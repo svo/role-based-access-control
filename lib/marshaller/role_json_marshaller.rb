a898ecb3c0c092fcd2c374f22caa6aec25570c03
SVO <svo@qual.is>
2021-03-21 14:50:56 +1100

    Starting to wire up the RoleJsonMarshaller
    Task: https://trello.com/c/uKS2xorz/

--- lib/marshaller/role_json_marshaller.rb
+++ lib/marshaller/role_json_marshaller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class RoleJsonMarshaller
+  def from_json(json)
+    JSON.parse json
+  end
 end
--- spec/marshaller/role_json_marshaller_spec.rb
+++ spec/marshaller/role_json_marshaller_spec.rb
@@ -3,4 +3,13 @@
 require "marshaller/role_json_marshaller"
 
 RSpec.describe RoleJsonMarshaller do
+  before(:each) do
+    @subject = described_class.new
+  end
+
+  it "converts a valid json array into the domain array" do
+    json = '{"Id":1,"Name":"System Administrator","Parent":0}'
+
+    expect(@subject.from_json(json)).to eq({ "Id" => 1, "Name" => "System Administrator", "Parent" => 0 })
+  end
 end
